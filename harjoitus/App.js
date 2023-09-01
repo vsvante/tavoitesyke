b82367d11398063012e5d5ea6a405e50b9cf68ab
Svante Vähäjylkkä <svante.vahajylkka@gmail.com>
2023-09-01 17:22:52 +0300

    Added working code

--- harjoitus/App.js
+++ harjoitus/App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [heartRateLimits, setHeartRateLimits] = useState('');
+
+  const calculateHeartRateLimits = () => {
+    if (age !== '') {
+      const ageNumber = parseInt(age);
+      const lower = Math.round((220 - ageNumber) * 0.65);
+      const upper = Math.round((220 - ageNumber) * 0.85);
+      setHeartRateLimits(`Hr limits: ${lower}-${upper}`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Enter your age to calculate heart rate limits</Text>
+      <Text>Age</Text>
+      <TextInput
+        style={styles.input} value={age} onChangeText={(text) => setAge(text)}
+        keyboardType="numeric"/>
+      <Button title="Calculate" onPress={calculateHeartRateLimits} />
+      <Text>{heartRateLimits}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +36,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    marginBottom: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginBottom: 10,
   },
 });
